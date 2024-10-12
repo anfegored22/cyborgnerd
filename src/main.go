ad84ece47516d20ae78bf2eab9be1149e16ac05c
Andres <anfego22@gmail.com>
2024-10-11 23:59:55 -0500

    Go back to embed!

--- src/main.go
+++ src/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"embed"
 	"html/template"
+	"io/fs"
 	"net/http"
 
 	"github.com/anfego22/cyborgnerd/cmd/api"
 )
 
+//go:embed templates/* static/*
+var content embed.FS
+
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
-	tmpl := template.Must(template.ParseGlob("templates/*.html"))
+	staticFS, _ := fs.Sub(content, "static")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(staticFS))))
+	tmpl := template.Must(template.ParseFS(content, "templates/*.html"))
 	cn := api.Server{Tmpl: tmpl}
 	cn.Start("8080")
+
 }
