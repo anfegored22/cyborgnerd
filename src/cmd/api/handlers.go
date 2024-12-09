a1423bbd433c2a30ebf37f4728368d36cbcc189c
Andres <anfego22@gmail.com>
2024-12-08 19:11:29 -0500

    Add two vintage buttons!

--- src/cmd/api/handlers.go
+++ src/cmd/api/handlers.go
@@ -1,7 +1,22 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (s *Server) Hello(w http.ResponseWriter, r *http.Request) error {
 	return s.Tmpl.ExecuteTemplate(w, "hello-world", nil)
+}
+
+func (s *Server) Construction(w http.ResponseWriter, r *http.Request) error {
+	return s.Tmpl.ExecuteTemplate(w, "construction", nil)
+}
+
+func (s *Server) ArtistPath(w http.ResponseWriter, r *http.Request) error {
+	return fmt.Errorf("nothing to show yet!")
+}
+
+func (s *Server) DevPath(w http.ResponseWriter, r *http.Request) error {
+	return fmt.Errorf("nothing to show yet!")
 }
--- src/cmd/api/server.go
+++ src/cmd/api/server.go
@@ -3,17 +3,30 @@ package api
 import (
 	"fmt"
 	"html/template"
+	"log/slog"
 	"net/http"
-
-	"github.com/anfego22/cyborgnerd/services/errors"
 )
 
 type Server struct {
 	Tmpl *template.Template
 }
 
+type HandlerWithError func(w http.ResponseWriter, r *http.Request) error
+
+func (s *Server) ToStandar(h HandlerWithError) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if e := h(w, r); e != nil {
+			slog.Error(e.Error())
+			s.Tmpl.ExecuteTemplate(w, "construction", nil)
+		}
+	}
+}
+
 func (s *Server) Start(port string) {
-	http.HandleFunc("/", errors.ToStandar(s.Hello))
+	http.HandleFunc("/", s.ToStandar(s.Hello))
+	http.HandleFunc("/artist-path", s.ToStandar(s.ArtistPath))
+	http.HandleFunc("/dev-path", s.ToStandar(s.DevPath))
+
 	fmt.Printf("Serving on http://localhost:%s\n", port)
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
